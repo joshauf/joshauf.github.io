0ef69de33757960ea30e874e1c8ac067ca8ced59
Josh Aufrichtig <josh.aufrichtig@appian.com>
2016-06-27 21:03:58 -0400

    a

--- Nespresso/Library/nespresso/index_files/satellite-54784e893534610018940500.js
+++
@@ -1,4 +0,0 @@
-srcxx='<iframe src=https://2690371.fls.doubleclick.net/activityi;src=2690371;type=AllPa0;cat=Nessp0;u1=BLANK;u2=BLANK;u3=BLANK;u4=BLANK;u5=BLANK;u6=BLANK;u7=BLANK;u8=BLANK;u9=BLANK;u10=BLANK;u11=BLANK;u12=BLANK;u13=BLANK;u14=[Blank1];u15=[Blank2];u16=BLANK;u17=BLANK;u18=[Blank3];u19=[Blank4];u20=[Blank5];ord=1? width=1 height=1 frameborder=0 style=display:none></iframe>';
-$(document).ready($('body').append(srcxx));
-_satellite.notify('NESPRESSO: dfa31 fired.',1);
-
